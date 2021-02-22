dc5733b8d371d69d7efb95ab7a83669d846e3894
Laiti Apoorva <laiti.apoorva@ibm.com>
2021-02-22 22:03:52 +0530

    added books

---
+++ SamplePrograms/my_first_go_program.go
@@ -0,0 +1,29 @@
+package main
+
+// To print word count in a given string
+import (
+	"fmt"
+	"strings"
+)
+
+func wordcount(s string) map[string]int {
+	var m map[string]int
+	m = make(map[string]int)
+	for _, val := range strings.Fields(s) {
+		// fmt.Println(val, ind)
+		_, ok := m[val]
+		// fmt.Println(each, ok)
+		if ok {
+			m[val] = m[val] + 1
+		} else {
+			m[val] = 1
+		}
+
+	}
+	return m
+}
+
+func main() {
+	// wc.Test(WordCount)
+	fmt.Println(wordcount("A man a plan a canal panama A"))
+}
